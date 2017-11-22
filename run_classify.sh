951e6cf13b408165f80cf3656a3d2aa4d958f75f
Min Soo Kim <minsk1@uci.edu>
2017-11-22 12:27:02 -0800

    Script to run classification in python

---
+++ run_classify.sh
@@ -0,0 +1,1 @@
+./python/classify.py input_file examples/imagenet/ilsvrc12_test_lmdb/ outputfile ./haha --model_def models/bvlc_alexnet/deploy.prototxt --pretrained_model bvlc_alexnet.caffemodel --mean_file models/bvlc_alexnet/imagenet_test_mean.binaryproto
