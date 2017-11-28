aa2e0cb5272c577d9641721d7806dc3933fe7810
Min Soo Kim <minsk1@uci.edu>
2017-11-28 11:07:04 -0800

    Script that successfully ran alexnet inference.

--- examples/imagenet/minsoo_create_imagenet.sh
+++ examples/imagenet/minsoo_create_imagenet.sh
@@ -7,7 +7,7 @@ EXAMPLE=examples/imagenet
 DATA=data/ilsvrc12
 TOOLS=build/tools
 
-TEST_DATA_ROOT=/home/minsoo/Imagenet/Test/
+VAL_DATA_ROOT=/home/minsoo/Imagenet/Val/
 
 # Set RESIZE=true to resize the images to 256x256. Leave as false if images have
 # already been resized using another tool.
@@ -21,22 +21,22 @@ else
 fi
 
 
-if [ ! -d "$TEST_DATA_ROOT" ]; then
-  echo "Error: TEST_DATA_ROOT is not a path to a directory: $TEST_DATA_ROOT"
-  echo "Set the TEST_DATA_ROOT variable in create_imagenet.sh to the path" \
+if [ ! -d "$VAL_DATA_ROOT" ]; then
+  echo "Error: VAL_DATA_ROOT is not a path to a directory: $VAL_DATA_ROOT"
+  echo "Set the VAL_DATA_ROOT variable in create_imagenet.sh to the path" \
        "where the ImageNet validation data is stored."
   exit 1
 fi
 
 
-echo "Creating test lmdb..."
+echo "Creating val lmdb..."
 
 GLOG_logtostderr=1 $TOOLS/convert_imageset \
     --resize_height=$RESIZE_HEIGHT \
     --resize_width=$RESIZE_WIDTH \
     --shuffle \
-    $TEST_DATA_ROOT \
-    $DATA/test.txt \
-    $EXAMPLE/ilsvrc12_test_lmdb
+    $VAL_DATA_ROOT \
+    $DATA/val.txt \
+    $EXAMPLE/ilsvrc12_val_lmdb
 
 echo "Done."
