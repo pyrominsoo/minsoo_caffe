b80bb127213c68bffd2fedee6da5b6da65e0fb7e
Min Soo Kim <minsk1@uci.edu>
2018-11-06 16:08:07 -0800

    Changed iteration size for resnet101

--- test_resnet101.sh
+++ test_resnet101.sh
@@ -3,4 +3,4 @@ set -e
 
 TOOLS=./build/tools
 
-$TOOLS/caffe test -model models/resnet/resnet101.prototxt -weights models/resnet/ResNet-101-model.caffemodel -iterations 10
+$TOOLS/caffe test -model models/resnet/resnet101.prototxt -weights models/resnet/ResNet-101-model.caffemodel -iterations 100
