8a0360a0ebccfdd089744f9b7ef157fb64512230
Min Soo Kim <minsk1@uci.edu>
2020-01-10 14:09:14 -0800

    For resnet inference scripts, changed the iterations to cover entire 50,000 images of ILSVRC2012

--- test_resnet101.sh
+++ test_resnet101.sh
@@ -3,4 +3,4 @@ set -e
 
 TOOLS=./build/tools
 
-$TOOLS/caffe test -gpu 0 -model models/resnet/resnet101.prototxt -weights models/resnet/ResNet-101-model.caffemodel -iterations 200
+$TOOLS/caffe test -gpu 0 -model models/resnet/resnet101.prototxt -weights models/resnet/ResNet-101-model.caffemodel -iterations 2000
--- test_resnet152.sh
+++ test_resnet152.sh
@@ -3,4 +3,4 @@ set -e
 
 TOOLS=./build/tools
 
-$TOOLS/caffe test -gpu 0 -model models/resnet/resnet152.prototxt -weights models/resnet/ResNet-152-model.caffemodel -iterations 500
+$TOOLS/caffe test -gpu 0 -model models/resnet/resnet152.prototxt -weights models/resnet/ResNet-152-model.caffemodel -iterations 5000
